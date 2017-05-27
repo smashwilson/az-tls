3fafaf7a1297207317a71e64ca196bb84d8af15e
Ash Wilson <smashwilson@gmail.com>
2017-05-26 23:10:09 -0400

    Only issue a new certificate within 7 days of expiry.

--- script/container/entrypoint.sh
+++ script/container/entrypoint.sh
@@ -2,7 +2,29 @@
 
 set -euo pipefail
 
-# TODO: use openssl to verify that the certificate needs renewal
+if [ "${FORCE:-}" != "yes" ]; then
+  printf "Checking current certificate's expiration date\n"
+  EXPIRATION_DATE=$(
+    echo |
+    openssl s_client -connect pushbot.party:443 2>/dev/null |
+    openssl x509 -noout -dates |
+    grep notAfter
+  )
+  EXPIRATION_DATE="${EXPIRATION_DATE#notAfter=}"
+  EXPIRATION_EPOCH=$(date +%s --date "${EXPIRATION_DATE}" -D '%b %e %H:%M:%S %Y')
+  NOW_EPOCH=$(date +%s)
+
+  let EXPIRATION_MS=EXPIRATION_EPOCH-NOW_EPOCH
+
+  if [ "${EXPIRATION_MS}" -gt 604800 ]; then
+    printf "Certificate expiration date %s is more than 7 days from now.\n" "${EXPIRATION_DATE}"
+    printf "Doing nothing.\n"
+    exit 0
+  fi
+
+  printf "Certificate expiration date %s is less than 7 days from now.\n" "${EXPIRATION_DATE}"
+  exit 1
+fi
 
 printf "Issuing new certificate\n"
 certbot certonly --manual --preferred-challenges=dns \
