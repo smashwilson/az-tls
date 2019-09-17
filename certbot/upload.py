3802aa1b7f965551ea2ac53dbc86775cd265b532
Ash Wilson <smashwilson@github.com>
2019-09-17 10:03:42 -0400

    Supply Authorization header myself

--- certbot/upload.py
+++ certbot/upload.py
@@ -3,11 +3,16 @@
 import os
 import json
 import urllib.request
+import base64
 
-password_manager = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-password_manager.add_password(None, os.environ['AZ_COORDINATOR_ADDR'], 'az-tls', os.environ['AZ_COORDINATOR_TOKEN'])
-auth_handler = urllib.request.HTTPBasicAuthHandler(password_manager)
-opener = urllib.request.build_opener(auth_handler)
+log_handler = urllib.request.HTTPSHandler()
+opener = urllib.request.build_opener(log_handler)
+
+def generate_auth_header():
+    pair = '{}:{}'.format('az-tls', os.environ['AZ_COORDINATOR_TOKEN']).encode('utf-8')
+    return 'Basic {}'.format(base64.b64encode(pair).decode('utf-8'))
+
+auth_header = generate_auth_header()
 
 tls_locations = {
     'TLS_DH_PARAMS': '/etc/letsencrypt/live/dhparams.pem',
@@ -24,7 +29,7 @@ print('Uploading TLS certificate secrets to the coordinator')
 secrets_req = urllib.request.Request(
     method='POST',
     url='{}/secrets'.format(os.environ['AZ_COORDINATOR_ADDR']),
-    headers={'Content-Type': 'application/json'},
+    headers={'Authorization': auth_header, 'Content-Type': 'application/json'},
     data=json.dumps(tls_secrets_payload).encode('utf-8')
 )
 opener.open(secrets_req)
@@ -32,7 +37,8 @@ opener.open(secrets_req)
 print('Triggering a coordinator sync')
 sync_req = urllib.request.Request(
     method='POST',
-    url='{}/sync'.format(os.environ(['AZ_COORDINATOR_ADDR']))
+    headers={'Authorization': auth_header},
+    url='{}/sync'.format(os.environ['AZ_COORDINATOR_ADDR'])
 )
 opener.open(sync_req)
 
