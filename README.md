a019f21e5461c01308478da505048ead1eb76c84
Ash Wilson <smashwilson@github.com>
2023-09-05 09:22:37 -0400

    Write a secrets file if no token or address is provided

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ Environment variables accepted for configuration:
 | --- | --- | --- |
 | `EMAIL` | Valid email address used to identify a Let's Encrypt account. | :star: **required** |
 | `AZ_COORDINATOR_ADDR` | Protocol, hostname, and port used to communicate with an `az-coordinator` daemon. Format: `https://hostname.net:8443`. | :star: **required** |
-| `AZ_COORDINATOR_TOKEN` | Authentication token to supply with `az-coordinator` requests. | :star: **required** |
+| `AZ_COORDINATOR_TOKEN` | Authentication token to supply with `az-coordinator` requests. | _empty_ |
 | `FORCE` | If `"yes"`, a new certificate will be issued even if the current one has not expired. | _empty_ |
 | `LE_PRODUCTION` | If `"yes"`, a production certificate will be issued instead of a staging one. | _empty_ |
+
+If `AZ_COORDINATOR_TOKEN` or `AZ_COORDINATOR_ADDR` are missing or blank, a secrets file compatible with `az-coordinator set-secrets` will be written to `/etc/letsencrypt/secrets.json` instead. Mount this path as a Docker volume to access the generated certificates.
--- certbot/upload.py
+++ certbot/upload.py
@@ -17,12 +17,6 @@ if os.environ.get('TRUST_LE_STAGING') == 'yes':
 log_handler = urllib.request.HTTPSHandler(context=ssl_context)
 opener = urllib.request.build_opener(log_handler)
 
-def generate_auth_header():
-    pair = '{}:{}'.format('az-tls', os.environ['AZ_COORDINATOR_TOKEN']).encode('utf-8')
-    return 'Basic {}'.format(base64.b64encode(pair).decode('utf-8'))
-
-auth_header = generate_auth_header()
-
 tls_locations = {
     'TLS_DH_PARAMS': '/etc/letsencrypt/live/dhparams.pem',
     'TLS_CERTIFICATE': '/etc/letsencrypt/live/backend.azurefire.net/fullchain.pem',
@@ -34,21 +28,33 @@ for varname, filepath in tls_locations.items():
     with open(filepath, encoding='utf8') as f:
         tls_secrets_payload[varname] = f.read()
 
-print('Uploading TLS certificate secrets to the coordinator')
-secrets_req = urllib.request.Request(
-    method='POST',
-    url='{}/secrets'.format(os.environ['AZ_COORDINATOR_ADDR']),
-    headers={'Authorization': auth_header, 'Content-Type': 'application/json'},
-    data=json.dumps(tls_secrets_payload).encode('utf-8')
-)
-opener.open(secrets_req)
+if os.environ.get('AZ_COORDINATOR_TOKEN') and os.environ['AZ_COORDINATOR_ADDR']:
+    def generate_auth_header():
+        pair = '{}:{}'.format('az-tls', os.environ['AZ_COORDINATOR_TOKEN']).encode('utf-8')
+        return 'Basic {}'.format(base64.b64encode(pair).decode('utf-8'))
 
-print('Triggering a coordinator sync')
-sync_req = urllib.request.Request(
-    method='POST',
-    headers={'Authorization': auth_header},
-    url='{}/sync'.format(os.environ['AZ_COORDINATOR_ADDR'])
-)
-opener.open(sync_req)
+    auth_header = generate_auth_header()
 
-print('Upload complete.')
+    print('Uploading TLS certificate secrets to the coordinator')
+    secrets_req = urllib.request.Request(
+        method='POST',
+        url='{}/secrets'.format(os.environ['AZ_COORDINATOR_ADDR']),
+        headers={'Authorization': auth_header, 'Content-Type': 'application/json'},
+        data=json.dumps(tls_secrets_payload).encode('utf-8')
+    )
+    opener.open(secrets_req)
+
+    print('Triggering a coordinator sync')
+    sync_req = urllib.request.Request(
+        method='POST',
+        headers={'Authorization': auth_header},
+        url='{}/sync'.format(os.environ['AZ_COORDINATOR_ADDR'])
+    )
+    opener.open(sync_req)
+
+    print('Upload complete.')
+else:
+    outfile = '/etc/letsencrypt/secrets.json'
+    with open(outfile, mode='w', encoding='utf8') as outf:
+        outf.write(json.dumps(tls_secrets_payload).encode('utf-8'))
+    print('Secrets written to file {}.'.format(outfile))
